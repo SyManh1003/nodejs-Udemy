697778352a85973a6d9cf3bb8bc25e158556c4f0
ManhProduce <thieusymanh2001@gmail.com>
2025-03-17 17:16:17 +0700

    review template engine

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 // const express = require("express");
 import express from "express";
-import "dotenv/config"
+import 'dotenv/config'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get("/", (req, res) => {
-    res.send("Hello world nodemon")
+    res.render("home")
 })
 
 app.listen(PORT, () => {
